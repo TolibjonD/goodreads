525078f26dfe94fe1dc4eae4b7ea4356ad263cd8
Tolibjon <stolibjon123@gmail.com>
2023-02-05 12:46:06 +0500

    True

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vk$#msu@e0cs-!)c&*_tw2o1&laz68e+o2czy2_j^^z!gi^f13'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
